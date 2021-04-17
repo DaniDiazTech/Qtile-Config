451638c84fd1e614f6456ec78bf62776ba3370ae
Daniel <danieldiazgonza52@gmail.com>
2021-04-16 23:05:01 -0500

    Added config for keybindings

---
+++ config_keybindings.py
@@ -0,0 +1,18 @@
+"""
+MODIFY THIS FILE TO CREATE CUSTOM KEYBINDINGS
+
+Keybindings are configured as dictionaries
+
+The command part 
+{'command': '', "keybinding": '', type=''}
+
+default_type 
+spawn = lazy.spawn()
+
+
+"""
+
+from libqtile.confreader import ConfigError
+
+from keys.default import *
+

214866594dcb50cdddce6dd11758c93a973e6c14
Daniel <danieldiazgonza52@gmail.com>
2021-04-16 23:04:21 -0500

    Modified floating confi

--- config.py
+++ config.py
@@ -8,31 +8,6 @@
 # |____/ |____/
 #
 
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
 
 ####### IMPORTS #########
 import os
@@ -42,10 +17,10 @@ import subprocess
 # from typing import List  # noqa: F401
 
 from libqtile import hook, layout
-from libqtile.config import Group
+from libqtile.config import Group, Match
 
 # Local Files
-from keybindings import Keybindings, Mouse
+from keys.keybindings import Mouse,Keybindings
 from widgets import MyWidgets
 from layouts import Layouts
 from groups import CreateGroups
@@ -76,35 +51,41 @@ if __name__ in ["config", "__main__"]:
     widgets_screen1 = obj_widgets.init_widgets_screen()
 
 
+
 dgroups_key_binder = None
-dgrups_app_rules = []
-main = None  # WARNING: this is deprecated and will be removed soon
+
+dgroups_app_rules = []  # type: List
+
 follow_mouse_focus = True
+
 bring_front_click = False
+
 cursor_warp = False
+
 
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
-    {'wmclass': 'confirm'},
-    {'wmclass': 'dialog'},
-    {'wmclass': 'download'},
-    {'wmclass': 'error'},
-    {'wmclass': 'file_progress'},
-    {'wmclass': 'notification'},
-    {'wmclass': 'splash'},
-    {'wmclass': 'toolbar'},
-    {'wmclass': 'xephyr'},
-    {'wmclass': 'Xephyr'},
-    {'wmclass': 'confirmreset'},  # gitk
-    {'wmclass': 'makebranch'},  # gitk
-    {'wmclass': 'maketag'},  # gitk
-    {'wname': 'branchdialog'},  # gitk
-    {'wname': 'pinentry'},  # GPG key password entry
-    {'wmclass': 'ssh-askpass'},  # ssh-askpass
+    *layout.Floating.default_float_rules,
+    Match(wm_class='confirmreset'),  # gitk
+    Match(wm_class='dialog'),  # Dialogs stuff
+    Match(wm_class='makebranch'),  # gitk
+    Match(wm_class='maketag'),  # gitk
+    Match(wm_class='ssh-askpass'),  # ssh-askpass
+    Match(title='branchdialog'),  # gitk
+    Match(title='pinentry'),  # GPG key password entry
 ])
 auto_fullscreen = True
+
 focus_on_window_activation = "smart"
 
+reconfigure_screens = True
+
+# If things like steam games want to auto-minimize themselves when losing
+# focus, should we respect this or not?
+respect_minimize_requests = True
+
+# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
+wmname = "LG3D"
 
 @hook.subscribe.startup_once
 def start_once():
@@ -115,13 +96,3 @@ def start_once():
 def dialogs(window):
     if(window.window.get_wm_type() == 'dialog' or window.window.get_wm_transient_for()):
         window.floating = True
-
-# XXX: Gasp! We're lying here. In fact, nobody really uses or cares about this
-# string besides java UI toolkits; you can see several discussions on the
-# mailing lists, GitHub issues, and other WM documentation that suggest setting
-# this string if your java app doesn't work correctly. We may as well just lie
-# and say that we're a working one by default.
-#
-# We choose LG3D to maximize irony: it is a 3D non-reparenting WM written in,
-# java that happens to be on java's whitelist.
-wmname = "LG3D"

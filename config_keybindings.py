40c64e716528e886eb22aa4c062de0849d468ad7
Daniel <danieldiazgonza52@gmail.com>
2021-04-22 16:49:09 -0500

    Added config structure

--- config_keybindings.py
+++ config_keybindings.py
@@ -1,18 +1,58 @@
 """
 MODIFY THIS FILE TO CREATE CUSTOM KEYBINDINGS
 
-Keybindings are configured as dictionaries
+Keybindings are configured with tuples, inside Predifined lists Variables
 
-The command part 
-{'command': '', "keybinding": '', type=''}
+Modifier -> list() -> Ex: [MOD, CONTROL]
 
-default_type 
-spawn = lazy.spawn()
+Key -> str() -> Ex: 'j'
 
+Command -> str() -> Ex: vscode
 
+(Modifier, Key, Command)
 """
 
 from libqtile.confreader import ConfigError
 
+# Import default mod keys
 from keys.default import *
 
+
+# Define constants here
+TERMINAL = "termite"
+
+
+# Basic window manager movements
+
+# ------------ Hardware Configs ------------
+HARDWARE_KEYS = [
+    # (Modifier, Key, Command)
+
+    # Volume
+    ([], "XF86AudioLowerVolume", "pactl set-sink-volume @DEFAULT_SINK@ -5%"),
+    ([], "XF86AudioRaiseVolume", "pactl set-sink-volume @DEFAULT_SINK@ +5%"),
+    ([], "XF86AudioMute", "pactl set-sink-mute @DEFAULT_SINK@ toggle"),
+     
+    # Brightness
+    ([], "XF86MonBrightnessUp", "brightnessctl set +10%"),
+    ([], "XF86MonBrightnessDown", "brightnessctl set 10%-"),
+]
+
+
+APPS = [
+    
+]
+
+##########################
+# Your custom keys here  #
+##########################
+
+CUSTOM_SPAWN_KEYS = [
+    
+]
+
+SPAWN_KEYS = HARDWARE_KEYS + APPS + CUSTOM_SPAWN_KEYS 
+
+SPAWN_CMD_KEYS = [
+    
+]
